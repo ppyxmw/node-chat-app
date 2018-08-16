142d4315276a36e080c88054be482e5c58e32a29
Michael <ppyxmw@gmail.com>
2018-08-16 09:40:49 +0000

    add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require("path"); // node module that simplifies paths
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "../public"); 
 // a simplified path is created.
@@ -9,11 +11,21 @@ var app = express();
 // app to config our express app. Remember we config express 
 // by calling methods on app (create routes, add middleware or start server).
 // instead of passing args to express
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath)); // serves up the public folder
 
-app.listen(port, () => { 
-    console.log(`server is up on port ${port}`); 
-    //starts up the server, getting port from line 6
-    // callback is just to print something for us in terminal
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
+
+server.listen(port, () => { 
+    console.log(`server is up on port ${port}`); 
+});
+//starts up the server, getting port from line 6
+// callback is just to print something for us in terminal
